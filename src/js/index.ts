873fec591cffe02a0da334f0127fb0e23064e569
Problocker <55052514+Problocker@users.noreply.github.com>
2020-10-22 13:44:04 +0200

    Små ændringer, nu virker HentAlle

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,21 +1,40 @@
+import axios, {
+    AxiosResponse,
+    AxiosError
+} from "../../node_modules/axios/index"
+
+interface IRecords{
+
+Id : number
+Title : string
+Artist : string
+Duration : number
+YearOfPublication : string
+CountryOrigin : string
+IsOnSpotify : boolean
+}
+
+let baseURL = "http://drrecordrest.azurewebsites.net/api/records";
+
 new Vue({
     // TypeScript compiler complains about Vue because the CDN link to Vue is in the html file.
     // Before the application runs this TypeScript file will be compiled into bundle.js
     // which is included at the bottom of the html file.
     el: "#app",
     data: {
-        name: "",
-        greeting: ""
+        records: []
     },
     methods: {
-        sayHello() {
-            console.log("Say Hello " + this.name)
-            if (this.name == "") {
-                this.greeting = "Hello NoName"
-            }
-            else {
-                this.greeting = "Hello " + this.name
-            }
+        GetAll() : void{
+            axios.get<IRecords[]>(baseURL)
+                .then((response: AxiosResponse<IRecords[]>) => {
+                    this.records = response.data;
+                    console.log(response.data)
+                })
+                .catch((error: AxiosError) => {
+                    console.log(error.message);
+                })
+
         }
     }
 })
